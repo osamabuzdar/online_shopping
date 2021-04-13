cd7b485caa1ea75e32469deafee2cf406b556ad5
osamabuzdar <48895360+osamabuzdar@users.noreply.github.com>
2021-04-13 21:27:08 +0500

    commit

--- lib/screen/homeScreen.dart
+++ lib/screen/homeScreen.dart
@@ -24,16 +24,26 @@ class HomeScreen extends StatelessWidget {
                   //mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
+                    // Padding(
+                    //   padding: const EdgeInsets.only(left: 20),
+                    //   child: Text(
+                    //     'Hey',
+                    //     style: TextStyle(color: Colors.white),
+                    //   ),
+                    // ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        Text(
-                          'Hey',
-                          style: TextStyle(
-                            fontSize: 25,
-                            color: Colors.white,
-                          ),
-                        ),
+                        // for multiple lines or for paragraph
+                        RichText(
+                            text: TextSpan(
+                                text: 'Hey ',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                                children: [
+                              TextSpan(
+                                text: 'Lets search your grocery food.',
+                              ),
+                            ])),
                         SizedBox(
                           width: 70,
                         ),
@@ -43,31 +53,230 @@ class HomeScreen extends StatelessWidget {
                         ),
                       ],
                     ),
-                    Text(
-                      'Lets search your grocery food',
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.white,
-                      ),
-                    ),
                   ],
                 ),
               ),
             ),
 
+            // second container of our screen which is basically white
             Container(
-              width: size.width,
-              height: size.height * 0.7,
-              decoration: BoxDecoration(
-                  color: Colors.white,
+                width: size.width,
+                height: size.height * 0.7,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade100,
                   borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(25),
                     topRight: Radius.circular(25),
-                  )),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    // upper container for categories
+                    MyParentCategoryContainer(size: size),
+
+                    // middle container with huge container and text
+                    Container(
+                      width: size.width,
+                      height: size.height * 0.3,
+                      child: Container(
+                        width: size.width,
+                        height: size.height * 0.2,
+                        child: Column(
+                          children: [
+                            // categories text and see all button
+                            SingleChildScrollView(
+                              scrollDirection: Axis.horizontal,
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  MiddleContainer(
+                                    size: size,
+                                    icon: Icon(
+                                      Icons.food_bank_sharp,
+                                      color: Colors.orange,
+                                      size: 50,
+                                    ),
+                                    categoryText: 'vegetabel'.toLowerCase(),
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
+// main home screen stateless widget ends here
+
+class MyParentCategoryContainer extends StatelessWidget {
+  const MyParentCategoryContainer({
+    Key key,
+    @required this.size,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Container(
+        width: size.width * 0.9,
+        height: size.height * 0.2,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: Column(
+          children: [
+            // categories text and see all button
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text('Categories'),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'see all',
+                    style: TextStyle(color: Colors.green.shade400),
+                  ),
+                )
+              ],
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  CategoryContainer(
+                    size: size,
+                    icon: Icon(
+                      Icons.food_bank,
+                      color: Colors.blue,
+                      size: 50,
+                    ),
+                    categoryText: 'fruits',
+                  ),
+                  CategoryContainer(
+                    size: size,
+                    icon: Icon(
+                      Icons.food_bank_sharp,
+                      color: Colors.orange,
+                      size: 50,
+                    ),
+                    categoryText: 'meat',
+                  ),
+                  CategoryContainer(
+                    size: size,
+                    icon: Icon(
+                      Icons.add,
+                      color: Colors.white,
+                      size: 50,
+                    ),
+                    categoryText: 'Drinks',
+                  ),
+                  CategoryContainer(
+                    size: size,
+                    icon: Icon(
+                      Icons.food_bank_sharp,
+                      color: Colors.orange,
+                      size: 50,
+                    ),
+                    categoryText: 'chocolate',
+                  ),
+                  CategoryContainer(
+                    size: size,
+                    icon: Icon(
+                      Icons.food_bank_sharp,
+                      color: Colors.orange,
+                      size: 50,
+                    ),
+                    categoryText: 'vegetabel'.toLowerCase(),
+                  ),
+                ],
+              ),
             )
           ],
         ),
       ),
+    );
+  }
+}
+
+class CategoryContainer extends StatelessWidget {
+  CategoryContainer({
+    this.size,
+    this.icon,
+    this.categoryText,
+  });
+
+  final Size size;
+  final icon;
+  final categoryText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Column(
+        children: [
+          Container(
+            width: size.width * 0.2,
+            height: size.height * 0.1,
+            decoration: BoxDecoration(
+              color: Colors.green.shade100,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: icon,
+          ),
+          Text('$categoryText')
+        ],
+      ),
+    );
+  }
+}
+
+class MiddleContainer extends StatelessWidget {
+  MiddleContainer({
+    this.size,
+    this.icon,
+    this.categoryText,
+  });
+
+  final Size size;
+  final icon;
+  final categoryText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Container(
+          width: size.width * 0.7,
+          height: size.height * 0.2,
+          decoration: BoxDecoration(
+            color: Colors.green.shade100,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Row(
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/images/food.png'),
+                radius: 60,
+              ),
+              Column(
+                children: [],
+              )
+            ],
+          )),
     );
   }
 }
